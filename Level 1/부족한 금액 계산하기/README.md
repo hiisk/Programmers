013b30df55e0329872269835ac4e61cd283d5f7a
hiisk <rkdgustjr10@gmail.com>
2021-11-13 14:53:25 +0900

    부족한 금액 계산하기

---
+++ Level 1/부족한 금액 계산하기/README.md
@@ -0,0 +1,2 @@
+# Programmers
+[부족한 금액 계산하기](https://hiisk.tistory.com/66)
---
+++ Level 1/부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,9 @@
+def solution(price, money, count):
+    answer = 0
+    for i in range(count):
+        answer += price*(i+1)
+    if answer>= money:
+        answer-=money
+    else:
+        answer = 0
+    return answer
